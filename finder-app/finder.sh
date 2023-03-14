0e9aa58a86989aae2790048c9dfdc81428d13451
dkoleary79 <dkoleary@hotmail.com>
2023-03-14 13:09:00 +0000

    My work to complete assignment 1

---
+++ finder-app/finder.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+status=0
+
+#first validate the args
+if [ $# -ne 2 ]
+then
+   echo "!ERR: Expected two arguments"
+   status=1
+else
+   dirName=$1
+   searchStr=$2
+   if ! [ -d $dirName ]
+   then
+      echo "!ERR: $dirName is NOT a directory"
+      status=1
+   fi
+fi
+
+#then perform the action
+if [ $status -eq 1 ]
+then
+    echo "Syntax: $0 <filesdir> <searchstr>"
+else
+    echo "The number of files are `find $dirName -type f | wc -l` and the number of matching lines are `grep -r $searchStr $dirName | wc -l`"
+fi
+
+exit $status
---
+++ finder-app/writer.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+status=0
+
+#first validate the args
+if [ $# -ne 2 ]
+then
+   echo "!ERR: Expected two arguments"
+   status=1
+else
+   filePath=$1
+   writeStr=$2
+fi
+
+#then perform the action
+if [ $status -eq 1 ]
+then
+    echo "Syntax: $0 <filePath> <writeStr>"
+else
+    mkdir -p "$(dirname "$filePath")" && touch "$filePath"
+    if [ $? -ne 0 ]
+    then
+        echo "!ERR: failed to create file"
+        status=1
+    else
+        echo "$2" > $filePath
+    fi
+fi
+
+exit $status
